e2aee9b4320cc5ee4e76fb50fe60cbd1a34b80f4
Petr Kubánek <pkubanek@lsst.org>
2021-03-03 17:51:25 -0300

    really add version files

---
+++ include/cRIO/version.h
@@ -0,0 +1,37 @@
+/*
+ * Developed for the Vera C. Rubin Observatory Telescope & Site Software Systems.
+ * This product includes software developed by the Vera C.Rubin Observatory Project
+ * (https://www.lsst.org). See the COPYRIGHT file at the top-level directory of
+ * this distribution for details of code ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef CRIO_version_H
+#define CRIO_version_H
+
+namespace LSST {
+namespace cRIO {
+
+/**
+ * Return cRIO version.
+ *
+ * @return version of cRIO library
+ */
+const char *version();
+
+}  // namespace cRIO
+}  // namespace LSST
+
+#endif /* CRIO_version_H */
