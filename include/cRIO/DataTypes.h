066db829f513469bb955aaba4af9f579714cd255
Petr Kubánek <pkubanek@lsst.org>
2023-11-08 10:52:13 +0000

    Expanded DataTypes.h

--- include/cRIO/DataTypes.h
+++ include/cRIO/DataTypes.h
@@ -1,27 +1,34 @@
 /*
- * Developed for the Vera C. Rubin Observatory Telescope & Site Software Systems.
- * This product includes software developed by the Vera C.Rubin Observatory Project
- * (https://www.lsst.org). See the COPYRIGHT file at the top-level directory of
- * this distribution for details of code ownership.
+ * This file is part of LSST M1M3 support system package.
  *
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * Developed for the Vera C. Rubin Telescope and Site System.
+ * This product includes software developed by the LSST Project
+ * (https://www.lsst.org).
+ * See the COPYRIGHT file at the top-level directory of this distribution
+ * for details of code ownership.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <https://www.gnu.org/licenses/>.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef DATATYPES_H_
 #define DATATYPES_H_
 
 #include <stdint.h>
+
+#define ACK_INPROGRESS 301  //* Acknowledges command reception, command is being executed.
+#define ACK_COMPLETE 303    //* Command is completed.
+#define ACK_FAILED -302     //* Command execution failed.
 
 /**
  * Number of ModBus subnets. Subnets are connected and commanded through cRIO
@@ -40,6 +47,15 @@
 
 /// Number of actuators in Z axis - shall equal to total number of actuators.
 #define FA_Z_COUNT 156
+
+/// Number of secondary (X or Y) actuators.
+#define FA_S_COUNT (FA_X_COUNT + FA_Y_COUNT)
+
+/// Maximal number of Force Actuator near neighbors
+#define FA_MAX_NEAR_COUNT 7
+
+/// Number of Force Actuator neighbors
+#define FA_FAR_COUNT 12
 
 /// Number of hardpoints.
 #define HP_COUNT 6
